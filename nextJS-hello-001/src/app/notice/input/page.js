5ef5b978c64e5edc830bb8caaa08c1cc0008da76
WHM0304 <whm0304@naver.com>
2024-02-28 12:58:06 +0900

    0228

--- nextJS-hello-001/src/app/notice/input/page.js
+++ nextJS-hello-001/src/app/notice/input/page.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-
+import styles from "@/app/notice/input/input.module.css";
 /**
  * 공지사항 작성하기
  * 작성자, 제목, 내용, 중요도
@@ -10,19 +9,25 @@ const NoticeInput = () => {
   return (
     <>
       <h1>공지사항작성하기</h1>
-      <input
-        type="text"
-        placeholder="작성자입력"
-        name="author"
-      ></input>
-      <input type="text" placeholder="제목" name="title"></input>
-      <input type="text" placeholder="내용" name="memo"></input>
-      <input
-        type="text"
-        placeholder="중요도"
-        name="important"
-      ></input>
-      <input type="button" value="저장"></input>
+      <div className={styles.inputBox}>
+        <input
+          type="search"
+          placeholder="작성자입력"
+          name="author"
+        ></input>
+        <input type="text" placeholder="제목" name="title"></input>
+        <input
+          type="textLongMessage"
+          placeholder="내용"
+          name="memo"
+        ></input>
+        <input
+          type="range"
+          placeholder="중요도"
+          name="important"
+        ></input>
+        <input type="button" value="저장"></input>
+      </div>
     </>
   );
 };
